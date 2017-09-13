da5a09b2a369e4f9dcde7ca13fb9af896e68d645
angelgar <agarciadlg@gmail.com>
2017-09-13 13:47:20 -0400

    testing

--- lecture3.R
+++ lecture3.R
@@ -4,3 +4,20 @@
 #
 # Lecture 3
 ##################################################################
+
+
+## set seed to ensure reproducibility
+set.seed(1234)
+
+## define x and y
+x = rnorm(1000)
+y = 1 + 2 * x + rnorm(1000, 0, .4)
+
+## histogram of x
+hist(x)
+
+## scatterplot of y against x
+plot(x, y)
+
+## Print out the scatterplot
+dev.print(pdf, "./scatter_plot.pdf", height = 4, width = 4)
